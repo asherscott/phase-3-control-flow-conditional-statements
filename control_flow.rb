93ae45e204cc02a0feaf8152394aad0c3e6d7599
asher scott <asher.scott6@gmail.com>
2022-04-04 12:04:23 -0500

    commit tree 6d43fd344b2d2ae3c68eb72144afd421b4aca2ab

--- control_flow.rb
+++ control_flow.rb
@@ -1,16 +1,38 @@
+require 'pry'
+
 def admin_login(username, password)
-  # your code here
+  username = username.downcase
+  if username == "admin" && password == "12345"
+    return "Access granted"
+  else
+    return "Access denied"
+  end
+  binding.pry
 end
 
 def hows_the_weather(temperature)
-  # your code here
+  return "It's brisk out there!" if temperature < 40
+  return "It's a little chilly out there!" if temperature < 65
+  return "It's too dang hot out there!" if temperature > 85
+  "It's perfect out there!"
 end
 
 def fizzbuzz(num)
-  # your code here
+  return "FizzBuzz" if num%3 == 0 && num%5 == 0
+  return "Fizz" if num%3 == 0
+  return "Buzz" if num%5 == 0
+  num
 end
 
 def calculator(operation, num1, num2)
-  # your code here
+  case operation
+  when '*' then num1 * num2
+  when '/' then num1 / num2
+  when '+' then num1 + num2
+  when '-' then num1 - num2
+  else
+    puts "Invalid operation!"
+    nil
+  end
 end
 
